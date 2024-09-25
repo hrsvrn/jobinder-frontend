5a6df117e17d22585067b3d56e48072c723e2a3b
bhavyabhardwaj28 <bhavyabhardwaj.business@gmail.com>
2024-09-26 03:45:58 +0530

    added home and signup

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,19 @@
 
 import logo1 from '../images/Jobinderr-3-removebg-preview.png'
 
-const Nav = ({minimal,authToken})=> {
+const Nav = ({minimal,authToken,setShowModal,showModal,setIsSignUp})=> {
+    const handleClick=()=>{
+        setShowModal(true)
+        setIsSignUp(false)//bec we are clicking on logged in so we will make it false
+        
+    }
   return (
     <nav>
     <div className="logo-container" >
         <img className="logo" src={minimal ? '' : logo1}></img>
       
     </div>
-    {!authToken && <button className='nav-button'> Log-In
+    {!authToken && <button className='nav-button' onClick={handleClick} disabled='{showModal}'> Log-In
        
    </button>}
     </nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import Nav from '../components/Nav';
 import { useState } from 'react';
 import AuthModal from '../components/AuthModal';
-import bg from '../images/bg1.png';
+import img from '../images/bg2.png';
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(true);
     const authToken = false;
 
     const handleClick = () => {
@@ -13,22 +14,46 @@ const Home = () => {
     };
 
     return (
-        <div>
-            <img src='../images/bg1.png'/>
-        <div className='overlay'>
-            <div className="home" >
-                
-                <Nav minimal={false} authToken={authToken} />
-                <h1>Swipe right</h1>
-                <p>And get Jobs with Jobinderr</p>
-                <button className='primary-button' onClick={handleClick}>
-                    {authToken ? 'Signout' : 'Create Account'}
-                </button>
-                {showModal && (
-                    <AuthModal setShowModal={setShowModal} />
-                )}
+        <div 
+            className="home-container" 
+            style={{
+                backgroundImage: `url(${img})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: '100vh',
+                filter: 'brightness(0.7)', // Lower the brightness to make the image darker
+                position: 'relative'
+            }}
+        >
+            {/* Overlay for additional effect */}
+            <div className='dark-overlay' style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0.7)', // Dark transparent overlay
+            }}></div>
+
+            <div className='overlay'>
+                <div className="home" style={{ position: 'relative', zIndex: 1 }}>
+                    <Nav minimal={false}
+                        authToken={authToken}
+                        setShowModal={setShowModal}
+                        setIsSignUp={setIsSignUp}
+                    />
+                    <h1 className="fade-in-text">Swipe right</h1>
+                    <p className="fade-in-text">And get Jobs with Jobinderr</p>
+                    <button className='primary-button' onClick={handleClick}>
+                        {authToken ? 'Signout' : 'Create Account'}
+                    </button>
+                    {showModal && (
+                        <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+                    )}
+                </div>
             </div>
-        </div>
         </div>
     );
 };
